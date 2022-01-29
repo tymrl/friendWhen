629b81edd3aa1cf41405d010e755a403a6d4c104
Tim Lewandowski <tim.lewandowski@gmail.com>
2022-01-29 13:38:21 -0500

    Use 2-space indentation

--- App.tsx
+++ App.tsx
@@ -1,40 +1,38 @@
-import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, FlatList, View } from 'react-native';
-import { mockData } from './src/Friend';
-import { FriendRow } from './src/FriendRow';
+import React, { useState } from "react";
+import { StyleSheet, SafeAreaView, FlatList, View } from "react-native";
+import { mockData } from "./src/Friend";
+import { FriendRow } from "./src/FriendRow";
 
 export default function App() {
-    const [friends, setFriends] = useState(mockData);
+  const [friends, setFriends] = useState(mockData);
 
-    return (
-        <SafeAreaView style={styles.root}>
-            <View style={styles.listContainer}>
-                <FlatList
-                    data={friends.sort(
-                        (a, b) => b.periodsElapsed - a.periodsElapsed
-                    )}
-                    renderItem={({ item }) => FriendRow({ friend: item })}
-                />
-                <View style={styles.spacer} />
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.root}>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={friends.sort((a, b) => b.periodsElapsed - a.periodsElapsed)}
+          renderItem={({ item }) => FriendRow({ friend: item })}
+        />
+        <View style={styles.spacer} />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    root: {
-        display: 'flex',
-        flexDirection: 'row',
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    listContainer: {
-        display: 'flex',
-        flex: 1,
-    },
-    spacer: {
-        flex: 1,
-    },
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listContainer: {
+    display: "flex",
+    flex: 1,
+  },
+  spacer: {
+    flex: 1,
+  },
 });

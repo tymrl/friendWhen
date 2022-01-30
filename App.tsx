e68bc3e8c422574d9840cbea51a46ed86f64381b
Tim Lewandowski <tim.lewandowski@gmail.com>
2022-01-29 19:29:42 -0500

    Add EditFriendScreen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { FriendList } from "./src/FriendList";
+import { EditFriendScreen } from "./src/EditFriendScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return FriendList();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="FriendList" component={FriendList} />
+        <Stack.Screen name="EditFriendScreen" component={EditFriendScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ src/EditFriendScreen.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+export const EditFriendScreen = () => {
+  //   const [friend, setFriend] = useState(mockData);
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text>Edit</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
